c5cba0eb79380e924f2e74d0287b5b5714f6c5ca
juanquiro <juanquirozsana@gmail.com>
2023-05-09 21:13:48 +0000

    Good 1

--- 01/app/src/app/components/Card.tsx
+++ 01/app/src/app/components/Card.tsx
@@ -3,12 +3,12 @@
 const Card = () => {
   return (
     <section
-      className={`mr-0 ml-0  sm:ml-auto sm:w-72 p-5 sm:mr-5 bg-purple-800 rounded-3xl`}
+      className={`mr-0 ml-0  sm:ml-auto sm:w-72 p-5 sm:mr-5 bg-gradient-to-b from-indigo-500 via-indigo-600 to-indigo-700 rounded-3xl`}
     >
       <p className="text-xl text-center capitalize text-white/70">
         Your Results
       </p>
-      <div className="flex items-center justify-center w-32 h-32 mx-auto my-5 rounded-full bg-purple-950">
+      <div className="flex items-center justify-center w-32 h-32 mx-auto my-5 rounded-full shadow-sm shadow-black/10 bg-gradient-to-t from-indigo-600 to-indigo-700">
         <div className="text-center">
           <p className="text-4xl font-extrabold text-white">76</p>
           <p className="text-white/50">of 100</p>
