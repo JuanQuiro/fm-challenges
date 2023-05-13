6996bd9c23d52781b9539b4a5fb1db2c7339534d
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2023-05-13 02:16:22 +0000

    Agrege el map en ejercicio 3

--- 03/app/src/Mapa.js
+++ 03/app/src/Mapa.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import { TileLayer, Marker, Popup } from 'react-leaflet';
+import { TileLayer, Marker, Popup, MapContainer } from 'react-leaflet';
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css'
 
+const markerIcon = Leaflet.divIcon({
+  html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>`
+})
 
-class Map extends Component {
-  render() {
-    const position = [51.505, -0.09];
+const Map=(lat,lng) => {
+  const position = [lat.lat, lat.lng]
 
-    return (
-      <Map center={position} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="Map data © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
-    );
-  }
+  return(
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{height:'500px'}}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position} icon={markerIcon}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+  )
 }
 
 export default Map;
--- 03/app/src/app/page.tsx
+++ 03/app/src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import {GeoData} from './apiTypes'
+import { GeoData } from './apiTypes'
 import axios from 'axios';
 import dynamic from 'next/dynamic';
 
 const Map = dynamic(() => import('../Mapa'), {
-  ssr:false
+  ssr: false
 })
 
 
@@ -29,21 +29,30 @@ const GeoInfo: React.FC = () => {
 
     fetchData();
   }, []);
-console.log(geoData);
+  console.log(geoData);
 
   return (
     <div>
-      
+
       {geoData ? (
         <>
-        <p>
-          Your IP address is {geoData.ip}. You are located in {geoData.location.city},{' '}
-          {geoData.location.country}.
-        </p>
-        <Map />
+          <h1>IP Address Tracker</h1>
+          <div className="form-control">
+            <div className="input-group">
+              <input type="text" placeholder="Search for any IP address or domain" className="input w-10 input-bordered" />
+              <button className="btn btn-square">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+              </button>
+            </div>
+          </div>
+          <p>
+            Your IP address is {geoData.ip}. You are located in {geoData.location.city},{' '}
+            {geoData.location.country}.
+          </p>
+          <Map lat={geoData.location.lat} lng={geoData.location.lng} />
         </>
       ) : (
-        <p>Loading...</p>
+        <p className='flex h-screen justify-center items-center text-center'>Loading...</p>
       )}
     </div>
   );
